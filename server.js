db6ccb4ed40f720fb6a88d6d97fe09a35d85ceb3
Spaida Nsabwe <spaidaliphava@gmail.com>
2025-10-27 00:25:36 +0200

    Add files via upload
    Signed-off-by: Spaida Nsabwe <spaidaliphava@gmail.com>

---
+++ server.js
@@ -0,0 +1,103 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import User from "./models/User.js";
+import Message from "./models/Message.js";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect("mongodb://127.0.0.1:27017/chat-app", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Track online users
+const onlineUsers = new Map();
+
+// Serve index.html
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: "./public" });
+});
+
+// API: Get private chat history
+app.get("/api/messages/:user1/:user2", async (req, res) => {
+  try {
+    const { user1, user2 } = req.params;
+    const messages = await Message.find({
+      $or: [
+        { sender: user1, receiver: user2 },
+        { sender: user2, receiver: user1 },
+      ],
+    }).sort({ createdAt: 1 });
+    res.json(messages);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to load messages" });
+  }
+});
+
+// API: Search online users
+app.get("/api/search-online", async (req, res) => {
+  try {
+    const { username } = req.query;
+
+    const users = await User.find({
+      username: { $regex: username, $options: "i" },
+      online: true,
+    }).select("username online");
+
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to search online users" });
+  }
+});
+
+// Socket.IO
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("user joined", async (username) => {
+    socket.username = username;
+    onlineUsers.set(username, socket.id);
+
+    await User.findOneAndUpdate({ username }, { online: true }, { upsert: true });
+    const users = await User.find({}, "username online");
+    io.emit("users list", users);
+    io.emit("chat message", { sender: "System", text: `${username} joined` });
+  });
+
+  socket.on("chat message", async (msg) => {
+    const newMessage = new Message(msg);
+    await newMessage.save();
+    io.emit("chat message", msg);
+  });
+
+  socket.on("private message", async (msg) => {
+    const { sender, receiver, text } = msg;
+    const newMessage = new Message({ sender, receiver, text });
+    await newMessage.save();
+
+    const receiverSocketId = onlineUsers.get(receiver);
+    if (receiverSocketId) io.to(receiverSocketId).emit("private message", msg);
+
+    socket.emit("private message", msg);
+  });
+
+  socket.on("disconnect", async () => {
+    if (socket.username) {
+      onlineUsers.delete(socket.username);
+      await User.findOneAndUpdate({ username: socket.username }, { online: false });
+      const users = await User.find({}, "username online");
+      io.emit("users list", users);
+      io.emit("chat message", { sender: "System", text: `${socket.username} left` });
+    }
+  });
+});
+
+server.listen(3000, () => console.log("Server running on port 3000"));
