105d1c8d5d6ddda7b212abe04d41f02de3f878b8
Spaida Nsabwe <spaidaliphava@gmail.com>
2025-10-27 01:03:33 +0200

    Add files via upload
    Signed-off-by: Spaida Nsabwe <spaidaliphava@gmail.com>

---
+++ massage.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  sender: { type: String, required: true },
+  receiver: { type: String }, // optional for public messages
+  text: { type: String, required: true },
+}, { timestamps: true });
+
+export default mongoose.model("Message", messageSchema);
