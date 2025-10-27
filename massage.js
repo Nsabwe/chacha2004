dc9871ac645a13bc30a8c4c848bab26f44a2cc46
Spaida Nsabwe <spaidaliphava@gmail.com>
2025-10-27 07:53:45 +0200

    Delete massage.js
    Signed-off-by: Spaida Nsabwe <spaidaliphava@gmail.com>

--- massage.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const messageSchema = new mongoose.Schema({
-  sender: { type: String, required: true },
-  receiver: { type: String }, // optional for public messages
-  text: { type: String, required: true },
-}, { timestamps: true });
-
-export default mongoose.model("Message", messageSchema);
